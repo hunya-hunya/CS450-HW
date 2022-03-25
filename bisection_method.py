86b6ba7f726339c74726b4267ca15cf4795d4899
8830_@naver.com <jessicayoon51@gmail.com>
2022-03-25 16:08:42 -0500

    first commit

---
+++ bisection_method.py
@@ -0,0 +1,12 @@
+# Implement the bisection method on the provided function f. 
+# Stop when your last two x values differ by less than 10^-13.
+# Print your x values at each iteration.
+
+# INPUT:
+# f, a function for which you should find a root
+# x0 and x1, two starting values
+
+# OUTPUT:
+# zero, your approximation to the zero of the function
+# A plot of f with your zero marked (already produced by the provided plotting code)
+
